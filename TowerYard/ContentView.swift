ee87cfbe788d3886b3b2f8360458fae37f6010d6
npoway <n.smoliar2013@gmail.com>
2026-06-30 20:36:42 +0300

    Initial Commit

---
+++ TowerYard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TowerYard
+//
+//  Created by Никита on 30.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TowerYard/TowerYardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TowerYardApp.swift
+//  TowerYard
+//
+//  Created by Никита on 30.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct TowerYardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
